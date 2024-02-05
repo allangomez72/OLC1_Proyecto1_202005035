0e6dbd0b55e2fe25b261fc1ffd0c00c3e0761231
Allan <allantime72@gmail.com>
2024-02-05 10:49:51 -0600

    este si

---
+++ Proyecto1_Compi/src/proyecto1_compi/Proyecto1_Compi.java
@@ -0,0 +1,11 @@
+
+package proyecto1_compi;
+
+public class Proyecto1_Compi {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+    }
+    
+}
